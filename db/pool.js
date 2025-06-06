1f69cf8af2563ecb7f012446524da726bf6b3739
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-06-06 14:39:14 -0600

    Fix: Use DATABASE_URL from env for PostgreSQL connection

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,10 @@
-const { Pool } = require("pg")
+const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL,
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
